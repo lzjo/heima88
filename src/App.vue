fa016c7c42eb47d4c614f3a90ba836e3296be213
Your Name <you@example.com>
2021-01-17 18:56:12 +0800

    项目起步-删除脚手架不相干内容

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="@/assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <tt></tt>
-  </div>
+  
 </template>
 
 <script>
-// import HelloWorld from '@/components/HelloWorld.vue'
-import tt from '@/components/tt.vue'
-
-
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld
-    tt
-  }
+
 }
 </script>
 
 <style>
-@import url("~@/assets/ceshi.css");
+
 </style>
