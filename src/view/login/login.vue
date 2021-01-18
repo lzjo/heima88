38467ea8f00a55f1b01df54cc33b91a70f866765
Your Name <you@example.com>
2021-01-18 22:21:46 +0800

    login布局

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="login">
-    我是登录页面
+    <div class="left"></div>
+    <div class="right">
+      <img src="@/assets/login_banner_ele.png" alt="">
+    
+    </div>
   </div>
 </template>
 
@@ -10,6 +14,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less">
+  .login{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 100%;
+    background: linear-gradient(
+      225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+    .left{
+      width: 478px;
+      height: 550px;
+      background: rgba(245, 245, 245, 1);
+      padding: 43px;
+    }
+  }
 </style>
