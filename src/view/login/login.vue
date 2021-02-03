d911de52bf9c2e66cc79304c0d9acdfac990ddac
Your Name <you@example.com>
2021-02-03 23:56:35 +0800

    头像验证

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -10,32 +10,49 @@
       <!-- 1:表单登录信息 -->
       <el-form :model="form" :rules="rules" ref="form">
         <el-form-item prop="phone">
-          <el-input v-model="form.phone" prefix-icon="el-icon-user" placeholder="请输入用户名"></el-input>
+          <el-input
+            v-model="form.phone"
+            prefix-icon="el-icon-user"
+            placeholder="请输入用户名"
+          ></el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input v-model="form.password" :show-password="true" prefix-icon="el-icon-lock" placeholder="请输入密码"></el-input>
+          <el-input
+            v-model="form.password"
+            :show-password="true"
+            prefix-icon="el-icon-lock"
+            placeholder="请输入密码"
+          ></el-input>
         </el-form-item>
         <el-form-item prop="code">
-            <el-row>
-              <el-col :span="16">
-                <el-input v-model="form.code" prefix-icon="el-icon-key" placeholder="请输入验证码"></el-input>
-              </el-col>
-              <el-col :span="8">
-              <img src="@/assets/img/key.jpg" class="key"  alt="">
-              </el-col>
-            </el-row>
+          <el-row>
+            <el-col :span="16">
+              <el-input
+                v-model="form.code"
+                prefix-icon="el-icon-key"
+                placeholder="请输入验证码"
+              ></el-input>
+            </el-col>
+            <el-col :span="8">
+              <img src="@/assets/img/key.jpg" class="key" alt="" />
+            </el-col>
+          </el-row>
         </el-form-item>
         <el-form-item class="xieyi">
-          <el-checkbox v-model="form.ischeck">我已阅读并同意
-          <el-link type="primary">用户协议</el-link>和
-          <el-link type="primary">用户协议</el-link>
+          <el-checkbox v-model="form.ischeck"
+            >我已阅读并同意 <el-link type="primary">用户协议</el-link>和
+            <el-link type="primary">用户协议</el-link>
           </el-checkbox>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" class="btn" @click="loginclick">登录</el-button>
-          <br>
-        <el-button type="primary" class="btn" @click="registerclick">注册</el-button>
-      </el-form-item>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="btn" @click="loginclick"
+            >登录</el-button
+          >
+          <br />
+          <el-button type="primary" class="btn" @click="registerclick"
+            >注册</el-button
+          >
+        </el-form-item>
       </el-form>
     </div>
     <div class="right">
@@ -46,47 +63,42 @@
 </template>
 
 <script>
-import register from './register.vue'
+import register from "./register.vue";
 export default {
-  components:{
-    register
+  components: {
+    register,
   },
   name: "login",
   data() {
     return {
-      rules:{
-        phone:[
-          {required:true,message:'请输入手机号',trigger:'change'}
+      rules: {
+        phone: [{ required: true, message: "请输入手机号", trigger: "change" }],
+        password: [
+          { required: true, message: "请输入密码", trigger: "change" },
+          { min: 6, max: 12, message: "请输入6到12位密码", trigger: "change" },
         ],
-        password:[
-          {required:true,message:'请输入密码',trigger:'change'},
-          {min:6,max:12,message:'请输入6到12位密码',trigger:'change'}
+        code: [
+          { required: true, message: "请输入验证码", trigger: "change" },
+          { min: 4, max: 4, message: "请正确输入验证码", trigger: "change" },
         ],
-        code:[
-          {required:true,message:'请输入验证码',trigger:'change'},
-          {min:4,max:4,message:'请正确输入验证码',trigger:'change'}
-        ]
       },
-      form:{
+      form: {
         phone: "",
         password: "",
         code: "",
-        ischeck:"",
-      }
+        ischeck: "",
+      },
     };
   },
-  mounted() {
-    alert(process.env.VUE_APP_URL)
-  },
   methods: {
-    loginclick(){
-      this.$refs.form.validate((res)=>{
-        this.$message.success(res+"");
-      })
+    loginclick() {
+      this.$refs.form.validate((res) => {
+        this.$message.success(res + "");
+      });
     },
-    registerclick(){
-      this.$refs.register.dialogVisible = true
-    }
+    registerclick() {
+      this.$refs.register.dialogVisible = true;
+    },
   },
 };
 </script>
@@ -125,14 +137,14 @@ export default {
         margin-left: 15px;
       }
     }
-    .key{
+    .key {
       width: 100%;
-      height: 40px
+      height: 40px;
     }
-    .btn{
+    .btn {
       width: 100%;
     }
-    .btn:nth-child(1){
+    .btn:nth-child(1) {
       margin-bottom: 26px;
     }
   }
--- src/view/login/register.vue
+++ src/view/login/register.vue
@@ -1,35 +1,137 @@
 <template>
-<el-dialog
-  :visible.sync="dialogVisible"
-  width="600px"
-  class="register"
-  :show-close="false">
-  <div slot="title" class="title">用户注册</div>
-</el-dialog>
+  <el-dialog
+    :visible.sync="dialogVisible"
+    width="600px"
+    class="register"
+    :show-close="false"
+  >
+    <el-form :model="form" label-width="80px" :rules="rules" ref="form">
+      <div slot="title" class="title">用户注册</div>
+      <el-form-item label="头像" prop="avatar">
+        <el-upload
+          class="avatar-uploader"
+          :action="baseUrl + '/uploads'"
+          name="image"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+        >
+          <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-form-item>
+    </el-form>
+    <div slot="" class="centerClick">
+      <el-button>取消</el-button>
+      <el-button type="primary" @click="confirmClick">确定</el-button>
+    </div>
+  </el-dialog>
 </template>
 
 <script>
 export default {
-    name: 'register',
-    data() {
-        return {
-            dialogVisible: false
-        };
+  name: "register",
+  data() {
+    return {
+      dialogVisible: false,
+      form: {
+        // 保存的图片链接
+        avatar: "",
+      },
+      // 验证规则
+      rules: {
+        avatar: [{ required: true, message: "请上传图片", trigger: "change" }],
+      },
+      //基地址
+      baseUrl: process.env.VUE_APP_URL,
+      //上传的 图片
+      imageUrl: "",
+    };
+  },
+  methods: {
+    // 图片上传成功后的方法
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+      console.log(res);
+      this.form.avatar = res.data.file_path;
+      this.$refs.form.validateField("avatar");
+      console.log(this.avatar);
     },
+    // 图片上传前的方法
+    beforeAvatarUpload(file) {
+      const isJPG =
+        file.type === "image/jpeg" ||
+        file.type === "image/png" ||
+        file.type === "image/gif";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 或者 PNG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
+    // 点击确定全局验证方法
+    confirmClick() {
+      this.$refs.form.validate((res) => {
+        console.log(res);
+      });
+    },
+  },
 };
 </script>
 
+<style lang="less" scoped>
+.register {
+  .title {
+    text-align: center;
+    height: 53px;
+    line-height: 53px;
+    background: rgba(3, 129, 249, 1);
+    color: white;
+  }
+  .centerClick {
+    text-align: center;
+  }
+}
+</style>
+
 <style lang="less">
-.register{
-    .title{
-        text-align: center;
-        height: 53px;
-        line-height: 53px;
-        background: rgba(3, 129, 249, 1);
-        color: white;
-    }
-    .el-dialog__header{
-        padding: 0;
-    }
+.register {
+  .el-dialog__header {
+    padding: 0;
+  }
+  .avatar-uploader {
+    width: 178px;
+    margin: 0 auto;
+  }
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .avatar-uploader .el-upload:hover {
+    border-color: #409eff;
+  }
+
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 }
 </style>
